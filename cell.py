d6397b0f23d670322afaa2ca5263ce83dff95e36
nikolaystankov1 <nikolay.stankov1@gmail.com>
2024-07-15 17:02:51 +0300

    show surrounded cells

--- cell.py
+++ cell.py
@@ -20,7 +20,6 @@ class Cell:
             location,
             width=12,
             height=4,
-            text=f'{self.x},{self.y}'
         )
         btn.bind('<Button-1>', self.left_click_actions) # Left Click
         btn.bind('<Button-3>', self.right_click_actions) # Right Click
@@ -29,8 +28,33 @@ class Cell:
 
 
     def left_click_actions(self, event):
-        print(event)
-        print('I am left cliked!')
+        if self.is_mine:
+            self.show_mine()
+        else:
+            self.show_cell()
+
+    def get_cell_by_axix(self, x, y):
+        for cell in Cell.all:
+            if cell.x == x and cell.y == y:
+                return cell
+
+
+    def show_cell(self):
+        surrounded_cells = [
+            self.get_cell_by_axix(self.x - 1, self.y -1),
+            self.get_cell_by_axix(self.x - 1, self.y),
+            self.get_cell_by_axix(self.x - 1, self.y + 1),
+            self.get_cell_by_axix(self.x, self.y - 1),
+            self.get_cell_by_axix(self.x + 1, self.y - 1),
+            self.get_cell_by_axix(self.x + 1, self.y),
+            self.get_cell_by_axix(self.x + 1, self.y + 1),
+            self.get_cell_by_axix(self.x, self.y + 1),
+        ]
+
+        print(surrounded_cells)
+
+    def show_mine(self):
+        self.cell_button_object.configure(bg='red')
 
     def right_click_actions(self, event):
         print(event)
@@ -40,7 +64,7 @@ class Cell:
     @staticmethod
     def randomize_mines():
         picked_cells = random.sample(
-            Cell.al, settings.MINES_COUNT
+            Cell.all, settings.MINES_COUNT
         )
         for picked_cell in picked_cells:
             picked_cell.is_mine = True
